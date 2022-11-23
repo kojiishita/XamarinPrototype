a5d75f8c03aadf1962872f713aeb10ac20d3c0c2
koji.ishita <koji.ishita@works-g.co.jp>
2022-11-23 10:28:14 +0900

    initial

---
+++ Test/Test/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Test.ViewModels;
+using Xamarin.Forms;
+
+namespace Test.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
